99f4a86031caa00fa9c577d6afd4c1f1d8d3b091
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 20:07:21 -0300

    <iomanip> - setprecision

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
@@ -6,13 +7,16 @@ int main(){
     //int número inteiro
     float b = 9.6;
     //float número de ponto flutuante ocupa 4 bytes ( aguenta 6-7 algarismos depois do ponto )
-    double c = 4.6;
+    double c = 4.7777777777;
     //double ocupa 8 bytes ( aguenta 15-16 algarismos depois do ponto )
-
 
     std::cout<<a<<std::endl;
     std::cout<<b<<std::endl;
     std::cout<<c<<std::endl;
+
+    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;;
+    //configura a precisão para 10 casas decimais
+
 
     return 0;
 }
