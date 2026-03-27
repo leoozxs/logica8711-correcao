8478bbc60ae27b3daed60610cf1ec94651b1ac8d
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 20:18:09 -0300

    Soma, Subtração, multiplicação, Divisão

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include<iostream>
-#include<iomanip>
 
 int main(){
 
-    int a = 8;
-    //int número inteiro
-    float b = 9.6;
-    //float número de ponto flutuante ocupa 4 bytes ( aguenta 6-7 algarismos depois do ponto )
-    double c = 4.7777777777;
-    //double ocupa 8 bytes ( aguenta 15-16 algarismos depois do ponto )
-
-    std::cout<<a<<std::endl;
-    std::cout<<b<<std::endl;
-    std::cout<<c<<std::endl;
-
-    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;;
-    //configura a precisão para 10 casas decimais
-
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-2<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
 
     return 0;
 }
