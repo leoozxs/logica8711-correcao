795d01be22a506b42a6a6a0e0e7a9584dae3ec33
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 20:55:30 -0300

    Soma, subt, mult, div, com variáveis ja impostas 2.0

--- main.cpp
+++ main.cpp
@@ -12,11 +12,10 @@ int main(){
    div = num1 / num2;
 
 
-   std::cout<<"8 + 2 = "<<soma<<std::endl;
-   std::cout<<"8 - 2 = "<<subt<<std::endl;
-   std::cout<<"8 * 2 = "<<mult<<std::endl;
-   std::cout<<"8 / 2 = "<<div<<std::endl;
-
+   std::cout<<num1<<" + "<<num2<<" = "<<soma<<std::endl;
+   std::cout<<num1<<" - "<<num2<<" = "<<subt<<std::endl;
+   std::cout<<num1<<" * "<<num2<<" = "<<mult<<std::endl;
+   std::cout<<num1<<" / "<<num2<<" = "<<div<<std::endl;
 
 
     return 0;
