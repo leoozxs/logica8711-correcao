c85211137f838fa11684d4dc39694666419514fd
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 19:40:59 -0300

    int, float e double

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string saudacao = "Boa noite, Leo ";
+    int a = 8;
+    //int número inteiro
+    float b = 9.6;
+    //float número de ponto flutuante ocupa 4 bytes ( aguenta 6-7 algarismos depois do ponto )
+    double c = 4.6;
+    //double ocupa 8 bytes ( aguenta 15-16 algarismos depois do ponto )
 
-    std::cout<<saudacao<<std::endl;
+
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
 
     return 0;
 }
