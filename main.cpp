c55a0d57cab54263ca3892611acbc3bf8dea7ded
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 21:31:45 -0300

    std::cin

--- main.cpp
+++ main.cpp
@@ -3,15 +3,12 @@
 
 int main(){
 
-   int idade = 18;
-   float altura = 1.66;
-   std::string nome = "Leonardo";
+    std::string nome;
 
+    std::cout<<"Qual seu nome: "<<std::endl;
+    std::cin>>nome;
 
-   
-   std::cout<<"Meu nome = "<<nome<<"."<<std::endl;
-   std::cout<<"Minha altura = "<<altura<<"m."<<std::endl;
-   std::cout<<"Minha idade = "<<idade<<"anos."<<std::endl;
+    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
 
 
 
