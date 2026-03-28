abe082c11399c3d26eda9d363c679c889edc2f40
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-03-27 21:13:34 -0300

    int, float, string / Nome, altura , Idade

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
 int main(){
 
-   int num1 = 8;
-   int num2 = 2;
-   int soma, subt, mult, div;
+   int idade = 18;
+   float altura = 1.66;
+   std::string nome = "Leonardo";
+
+
    
-   soma = num1 + num2;
-   subt = num1 - num2;
-   mult = num1 * num2;
-   div = num1 / num2;
+   std::cout<<"Meu nome = "<<nome<<"."<<std::endl;
+   std::cout<<"Minha altura = "<<altura<<"m."<<std::endl;
+   std::cout<<"Minha idade = "<<idade<<"anos."<<std::endl;
 
-
-   std::cout<<num1<<" + "<<num2<<" = "<<soma<<std::endl;
-   std::cout<<num1<<" - "<<num2<<" = "<<subt<<std::endl;
-   std::cout<<num1<<" * "<<num2<<" = "<<mult<<std::endl;
-   std::cout<<num1<<" / "<<num2<<" = "<<div<<std::endl;
 
 
     return 0;
+
 }
 
