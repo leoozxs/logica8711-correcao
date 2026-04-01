a6fb7c71523d5c70cb5a986c38137b39d2e99fed
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-01 19:50:23 -0300

    Função - auto

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 
 int main(){
 
-    char letra = 'E';
+    auto num1 = 10;
+    auto num2 = 15.80f;
+    auto letra = 'E';
+    auto texto = "Leonardo";
+
+    std::cout<<num1<<std::endl;
+    std::cout<<num2<<std::endl;
+    std::cout<<letra<<std::endl;
+    std::cout<<texto<<std::endl;
 
 
-  std::cout<<letra<<std::endl;
-  
+
+
 
     return 0;
 }
