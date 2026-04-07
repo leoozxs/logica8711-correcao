4c8ad23980c08824a55adf31b45c012a0a70b083
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-06 21:56:03 -0300

    função array e at

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 
 int main(){
 
-   int numeros[5] = {10,20,30,40,50};
+    std::string nome = "Leonardo";
 
-   std::cout<<numeros[1];
-   
+    std::cout<<nome[0]<<std::endl;
+    std::cout<<nome[2]<<std::endl;
+    std::cout<<nome[nome.length() - 1]<<std::endl;
+    nome[0] = 'A';
+    std::cout<<nome<<std::endl;
+    std::cout<<nome.at(0)<<std::endl;
+
+
+
 
     return 0;
 }
