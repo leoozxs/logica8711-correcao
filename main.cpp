a505087918f32713d6629e5ecb065b3d9b2c03ff
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-06 21:18:07 -0300

    length / size - Contador de letras

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 
 int main(){
 
-    int a = 40;
-    std::string d = "10";
+   std::string nome = "Leo";
 
-    std::cout<<a+d<<std::endl;
-    
+   std::cout<<"Nome tem: "<<nome.length()<<" letras."<<std::endl;
+   std::cout<<"Nome tem: "<<nome.size()<<" letras."<<std::endl;
 
     return 0;
 }
