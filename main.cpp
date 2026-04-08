e9a72e170d1f0016552c5090c9970c3bdb8f1009
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-08 20:34:05 -0300

    std::max = comprar inteiros e informar qual o maior

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include<iostream>
-#include<string>
+#include<cmath>
+
 
 int main(){
 
-    int valor;
-    float porcentagem; 
-    float resultado;
+    int a = 5;
+    int b = 3;
 
-    std::cout<<"Informe o valor (Inteiro): "<<std::endl;
-    std::cin>>valor;
-
-    std::cout<<"Infrome a porcentagem que deseja descobrir do valor informado"<<std::endl;
-    std::cin>>porcentagem;
-
-    resultado = valor * (porcentagem / 100.0);
-    
-    std::cout<<"Valor informado: "<<valor<<std::endl;
-    std::cout<<"Porcentagem desejada: "<<porcentagem<<"%"<<std::endl;
-    std::cout<<"O Resultado e: "<<resultado<<std::endl;
-
+    std::cout<<std::max(a, b)<<std::endl;
 
 
 
