69508b9a56f91a5ff1d89fc60bc9f8b001274295
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-08 20:10:33 -0300

    Programa para descobrir porcentagem de numero informado

--- main.cpp
+++ main.cpp
@@ -3,14 +3,21 @@
 
 int main(){
 
-    std::string nome = "Leonardo";
+    int valor;
+    float porcentagem; 
+    float resultado;
 
-    std::cout<<nome[0]<<std::endl;
-    std::cout<<nome[2]<<std::endl;
-    std::cout<<nome[nome.length() - 1]<<std::endl;
-    nome[0] = 'A';
-    std::cout<<nome<<std::endl;
-    std::cout<<nome.at(0)<<std::endl;
+    std::cout<<"Informe o valor (Inteiro): "<<std::endl;
+    std::cin>>valor;
+
+    std::cout<<"Infrome a porcentagem que deseja descobrir do valor informado"<<std::endl;
+    std::cin>>porcentagem;
+
+    resultado = valor * (porcentagem / 100.0);
+    
+    std::cout<<"Valor informado: "<<valor<<std::endl;
+    std::cout<<"Porcentagem desejada: "<<porcentagem<<"%"<<std::endl;
+    std::cout<<"O Resultado e: "<<resultado<<std::endl;
 
 
 
