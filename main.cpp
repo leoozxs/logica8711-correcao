749d8ee108bb5e13669cf233c29d1b767f75577c
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-08 21:35:02 -0300

    função if e else

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 
 int main(){
 
-  std::cout<<std::sqrt(64)<<std::endl;
-  std::cout<<std::round(2.6)<<std::endl;
-  std::cout<<std::log(2)<<std::endl;
+  int a = 5;
+  int b =2;
+
+  if(a > 2){
+    std::cout<<"A e maior que B"<<std::endl;
+  }
+  else{
+    std::cout<<"B e maior que A"<<std::endl;
+  }
 
 
     return 0;
