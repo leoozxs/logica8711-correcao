ffbe4bfb93493b2f43f035a76834fcca6900344e
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-13 20:25:19 -0300

    Aplicando if e else if

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include<iostream>
-#include<string>
 
 
 int main(){
 
-    int idade;
+    int opcaoPagamento;
+    int qtdParafuso;
+    float valorParafuso = 1.40;
+    float valorTotalParafuso;
+    float desconto;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+    std::cout<<"Digite a quantidade de parafusos desejada: "<<std::endl;
+    std::cin>>qtdParafuso;
 
-    if(idade < 0){
-        std::cout<<"Idade inválida!"<<std::endl;
-    }else if(idade <= 2){
-        std::cout<<"Voce e um bebe!"<<std::endl;
-    }else if(idade <= 12){
-        std::cout<<"Voce e uma crianca!"<<std::endl;
-    }else if(idade <= 14){
-        std::cout<<"Voce e um pre-adolecente!"<<std::endl;
-    }else if(idade <= 17){
-        std::cout<<"Voce e um adolecente!"<<std::endl;
-    }else if(idade >= 18){
-        std::cout<<"Voce e um adulto!"<<std::endl;
+    valorTotalParafuso = qtdParafuso * valorParafuso;
+
+    std::cout<<"Qual a forma de pagamento: "<<std::endl;
+    std::cout<<"--- 1 - Pix"<<std::endl;
+    std::cout<<"--- 2 - Dinheiro"<<std::endl;
+    std::cout<<"--- 3 Cartão"<<std::endl;
+    std::cin>>opcaoPagamento;
+
+    if(opcaoPagamento == 1){
+        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }else if (opcaoPagamento == 2){
+        desconto = valorTotalParafuso * 0.90;
+        std::cout<<"Sua compra ficou no valor total de R$: "<<desconto<<std::endl;
+    }else if(opcaoPagamento == 3){
+        std::cout<<"SUa compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }else{
+        std::cout<<"Valor invalido"<<std::endl;
     }
-    
+
+
     return 0;
 }
