58ddf5ce6fa02945dd331fde08c31ce08d001757
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-15 19:19:50 -0300

    funcao for(loop)  = imprime algo quantas vezes quiser

--- main.cpp
+++ main.cpp
@@ -5,41 +5,8 @@
 
 int main(){
 
-    std::string nome;
-    float altura;
-    int idade;
-
-    std::cout<<"Boa noite! Qual seu nome? "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual sua altura? "<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Qual sua idade? "<<std::endl;
-    std::cin>>idade;
-
-    if(idade <= 0){
-        std::cout<<"Idade inválida."<<std::endl;
-    }else if (idade <= 10){
-        std::cout<<"Boa noite! "<<nome<<"."<<std::endl;
-        std::cout<<"Sua idade = "<<idade<<" anos."<<std::endl;
-        std::cout<<"Voce se enquadra na classificacao 'Infantil'."<<std::endl;
-        std::cout<<"Sua altura = "<<altura<<"m."<<std::endl;
-    }else if (idade <= 18){
-        std::cout<<"Boa noite! "<<nome<<"."<<std::endl;
-        std::cout<<"Sua idade = "<<idade<<" anos."<<std::endl;
-        std::cout<<"Voce se enquadra na classificacao 'Adolescente'."<<std::endl;
-        std::cout<<"Sua altura = "<<altura<<"m."<<std::endl;
-    }else if(idade <= 60){
-        std::cout<<"Boa noite! "<<nome<<"."<<std::endl;
-        std::cout<<"Sua idade = "<<idade<<" anos."<<std::endl;
-        std::cout<<"Voce se enquadra na classificacao 'Adulto'."<<std::endl;
-        std::cout<<"Sua altura = "<<altura<<"m."<<std::endl;
-    }else if(idade >= 61){
-        std::cout<<"Boa noite! "<<nome<<"."<<std::endl;
-        std::cout<<"Sua idade = "<<idade<<" anos."<<std::endl;
-        std::cout<<"Voce se enquadra na classificacao 'Idoso'."<<std::endl;
-        std::cout<<"Sua altura = "<<altura<<"m."<<std::endl;
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"Este é um loop!!"<<std::endl;
     }
 
     return 0;
