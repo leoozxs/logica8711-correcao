0bd56bdc75f2dac4e038e2429b9b2a2adce29236
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-15 19:45:29 -0300

    funcao for(loop) controlada por segundos

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
-#include<string>
-
-//Atividade, algortimo Nome, Altura, Idade, Infantil, Adolecente, Adulto. = Imprimir tudo
+#include<thread> // para lidar com o tempo de execução
+#include<chrono> //para definir unidades de tempo (mds, segundos)
 
 int main(){
 
     for(int i = 10; i >= 0; i--){
+        
         std::cout<<"A bomba ira explodir em..."<<i<<std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    
 
     return 0;
 }
