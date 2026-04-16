e5ce5c5fc1cfb77df42a53a10f55a1c19b8007c6
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-15 21:37:23 -0300

    for(int)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,12 @@
 #include <iostream>
 
-int main()
-{
+int main(){
 
-    int opcao;
-
-    std::cout << "Digite um valor para opção: " << std::endl;
-    std::cin >> opcao;
-
-    switch (opcao)
-    {
-    case 1:
-        std::cout << "Domingo!" << std::endl;
-        break;
-    case 2:
-        std::cout << "Segunda-feira!" << std::endl;
-        break;
-    case 3:
-        std::cout << "Terça-feira!" << std::endl;
-        break;
-    case 4:
-        std::cout << "Quarta-feira!" << std::endl;
-        break;
-    case 5:
-        std::cout << "Quinta-feira!" << std::endl;
-        break;
-    case 6:
-        std::cout << "Sexta-feira" << std::endl;
-        break;
-    case 7:
-        std::cout << "Sábado" << std::endl;
-        break;
-    default:
-        std::cout << "Número inválido!" << std::endl;
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for(int num : myNumbers){
+        std::cout<<num<<std::endl;
     }
+    
 
     return 0;
 }
