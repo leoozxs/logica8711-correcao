7cf2dca4dcbf1d0a1af28789c9ce9443ba63db26
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-20 19:42:09 -0300

    Array

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
-    int myNumbers[5] = {10, 20, 30, 40, 50};
-    for(int num : myNumbers){
-        std::cout<<num<<std::endl;
-    }
-    
+    std::string nomes[3] = {"João", "Paula", "Murilo"};
+    int idades [3] = {27, 31 , 20};
 
+    std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos."<<std::endl;
+    std::cout<<nomes[2]<<" tem "<<idades[2]<<" anos."<<std::endl;
+
+
+ 
     return 0;
 }
