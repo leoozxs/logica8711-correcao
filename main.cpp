63c89c1832453b47d5220fd5e47585446ca7b846
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-28 20:12:35 -0300

    void myFunction fora do int()main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
+    void myFunction(){
+        std::cout<<"Ola mundo!"<<std::endl;
+    }
+
 int main(){
     
-    int myNumbers[5] = {1, 2, 3, 4, 5};
-
-    for(int i = 0; i < 5; i++){
-        std::cout<<myNumbers[i]<<std::endl;
-    }
+     myFunction();
 
     return 0;
 }
