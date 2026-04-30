7e8567d2e040f8ad4e3ae247f0e4ac219296acc8
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-30 20:47:28 -0300

    PORCENTAGEM - IMPAR / PAR

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
-#include<string>
+#include<iomanip>
 
 
 int main(){
 
-    int choice;
-    float temp, converted;
+    int num;
 
-    std::cout<<"------------ Temperature Converter ---------------"<<std::endl;
-    std::cout<<"1: Celcius to Fahrenheit"<<std::endl;
-    std::cout<<"2: Fahrenheit to Celsius"<<std::endl;
-    std::cout<<"Choose an option (1 or 2): "<<std::endl;
-    std::cin>>choice;
-    
+    std::cout<<"Digite um numero: "<<std::endl;
+    std::cin>>num;
 
-    if(choice == 1){
-        std::cout<<"Enter temperature in Celsius: "<<std::endl;
-        std::cin>>temp;
-        converted = (temp * 1.8 ) + 32;
-        std::cout<<temp<<"C is "<<converted<<" F."<<std::endl;
-    }
-    else if(choice == 2){
-        std::cout<<"Enter temperature in Fahrenheit: "<<std::endl;
-        std::cin>>temp;
-        converted = (temp - 32) / 1.8;
-        std::cout<<temp<<"F is "<<converted<<" C."<<std::endl;
+
+    if(num % 2 == 0){
+        std::cout<<"Par"<<std::endl;
     }
     else{
-        std::cout<<"Invalid Option! Please restart."<<std::endl;
+        std::cout<<"Impar"<<std::endl;
     }
-
 
     return 0;
 }
