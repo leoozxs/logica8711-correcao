56dbffea6a59023f6b2746679adc4341f9b00463
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-04-30 19:48:39 -0300

    Código Conversor de Tempertatura - Celsius/Fahrenheit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include<string>
 
-int myFunction(int x, int y){
-    return x + y;
-}
 
 int main(){
 
-    std::cout<<myFunction(5, 3)<<std::endl;   
+    int choice;
+    float temp, converted;
+
+    std::cout<<"------------ Temperature Converter ---------------"<<std::endl;
+    std::cout<<"1: Celcius to Fahrenheit"<<std::endl;
+    std::cout<<"2: Fahrenheit to Celsius"<<std::endl;
+    std::cout<<"Choose an option (1 or 2): "<<std::endl;
+    std::cin>>choice;
     
+
+    if(choice == 1){
+        std::cout<<"Enter temperature in Celsius: "<<std::endl;
+        std::cin>>temp;
+        converted = (temp * 1.8 ) + 32;
+        std::cout<<temp<<"C is "<<converted<<" F."<<std::endl;
+    }
+    else if(choice == 2){
+        std::cout<<"Enter temperature in Fahrenheit: "<<std::endl;
+        std::cin>>temp;
+        converted = (temp - 32) / 1.8;
+        std::cout<<temp<<"F is "<<converted<<" C."<<std::endl;
+    }
+    else{
+        std::cout<<"Invalid Option! Please restart."<<std::endl;
+    }
+
+
     return 0;
 }
  
