89c7a257507f2a24c51da6c1387ba25089687eaf
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-05-12 19:49:22 -0300

    Calculadora simples com stiwch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(){
 
     int num1, num2, soma, subt, mult;
     float div;
+    int op;
 
     std::cout<<"Digite o valor para o numero 1: "<<std::endl;
     std::cin>>num1;
@@ -11,15 +12,34 @@ int main(){
     std::cout<<"Digite aqui o valor para o numero 2: "<<std::endl;
     std::cin>>num2;
 
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
-    div = num1 / num2;
+    std::cout<<"\n-------- Escolha a operacao --------"<<std::endl;
+    std::cout<<"1 - Soma"<<std::endl;
+    std::cout<<"2 - Subrtracao"<<std::endl;
+    std::cout<<"3 - Multiplicacao"<<std::endl;
+    std::cout<<"4 - Divisao"<<std::endl;
+    std::cin>>op;
 
-    std::cout<<"O resultado da soma e = "<<soma<<std::endl;
-    std::cout<<"O resultado a subtracao e ="<<subt<<std::endl;
-    std::cout<<"O resultado da multiplicacao e = "<<mult<<std::endl;
-    std::cout<<"O resultado da divisao e = "<<div<<std::endl;
+    switch(op){
+        case 1:
+        soma = num1 + num2;
+        std::cout<<"O resultado da soma e = "<<soma<<std::endl;
+        break;
+        case 2:
+        subt = num1 - num2;
+        std::cout<<"O resultado da subtracao e = "<<subt<<std::endl;
+        break;
+        case 3:
+        mult = num1 * num2;
+        std::cout<<"O resultado da multiplicacao e = "<<mult<<std::endl;
+        case 4:
+        break;
+        div = num1 / num2;
+        std::cout<<"O resultado da divisao e = "<<div<<std::endl;
+        break;
+        default:
+        std::cout<<"Voce nao digitou um valor valido!"<<std::endl;
+        
+    }
 
     return 0;
     }
