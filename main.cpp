6dc0e80eda2ca20b2c25ddcb9a88ce2b9ac8da20
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-05-26 19:36:24 -0300

    interpolação de vartiaveis

--- main.cpp
+++ main.cpp
@@ -1,45 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <format>
+#include <string>
+ 
 
+ 
 int main(){
 
-    int num1, num2, soma, subt, mult;
-    float div;
-    int op;
+    std::string nome = "Eduardo";
 
-    std::cout<<"Digite o valor para o numero 1: "<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"Digite aqui o valor para o numero 2: "<<std::endl;
-    std::cin>>num2;
-
-    std::cout<<"\n-------- Escolha a operacao --------"<<std::endl;
-    std::cout<<"1 - Soma"<<std::endl;
-    std::cout<<"2 - Subrtracao"<<std::endl;
-    std::cout<<"3 - Multiplicacao"<<std::endl;
-    std::cout<<"4 - Divisao"<<std::endl;
-    std::cin>>op;
-
-    switch(op){
-        case 1:
-        soma = num1 + num2;
-        std::cout<<"O resultado da soma e = "<<soma<<std::endl;
-        break;
-        case 2:
-        subt = num1 - num2;
-        std::cout<<"O resultado da subtracao e = "<<subt<<std::endl;
-        break;
-        case 3:
-        mult = num1 * num2;
-        std::cout<<"O resultado da multiplicacao e = "<<mult<<std::endl;
-        case 4:
-        break;
-        div = num1 / num2;
-        std::cout<<"O resultado da divisao e = "<<div<<std::endl;
-        break;
-        default:
-        std::cout<<"Voce nao digitou um valor valido!"<<std::endl;
-        
-    }
-
+    std::string mensagem = std::format("Ola, {}! Voce tem {} anos. \n", nome);
+    std::cout<<mensagem; 
+    
     return 0;
-    }
+}
+ 
--- main1.cpp
+++ main1.cpp
@@ -3,6 +3,125 @@
 #include <iomanip>
 #include <windows.h>
 
+
+void infoModelagem(){
+
+    int op;
+
+        do{
+        std::cout<<"\n==================================================="<<std::endl;
+        std::cout<<"========== CURSO: Técnico em Modelagem ==========="<<std::endl;
+        std::cout<<"=================================================="<<std::endl;
+        std::cout<<"\nValor Total:"<<std::endl;
+        std::cout<<"R$ 14.844,79"<<std::endl;
+        std::cout<<"\nParcelamento:"<<std::endl;
+        std::cout<<"Até 28x"<<std::endl;
+        std::cout<<"\nDocentes:"<<std::endl;
+        std::cout<<"- Natalia"<<std::endl;
+        std::cout<<"- Ramon"<<std::endl;
+        std::cout<<"\nHorários:"<<std::endl;
+        std::cout<<"- Segunda, quarta e sexta"<<std::endl;
+        std::cout<<"- Das 19h às 22h"<<std::endl;
+        std::cout<<"\nou"<<std::endl;
+        std::cout<<"\n- Terça e sexta"<<std::endl;
+        std::cout<<"- Das 19h às 22h"<<std::endl;
+        std::cout<<"\nCarga Horária:"<<std::endl;
+        std::cout<<"800 horas"<<std::endl;
+        std::cout<<"\nGostaria de saber as possíveis formas de formas de pagamento?\n1 - SIM ( EM DESENVOLVIMENTO )\n2 - NÃO "<<std::endl;
+        std::cin>>op;
+
+            switch(op){
+
+            case 1:
+            std::cout<<"Em desenvolvimento"<<std::endl;
+            exit(0);
+
+            case 2:
+            system("cls");
+            return;
+
+            default:
+            std::cout<<"Opção inválida!"<<std::endl;
+        }
+    }while(true);
+}
+void infoEnfermagem(){
+
+    int op;
+
+        do{
+        std::cout<<"\n==================================================="<<std::endl;
+        std::cout<<"========== CURSO: Técnico em Enfermagem =========="<<std::endl;
+        std::cout<<"=================================================="<<std::endl;
+        std::cout<<"\nValor Total:"<<std::endl;
+        std::cout<<"R$ 15.278,00"<<std::endl;
+        std::cout<<"\nParcelamento:"<<std::endl;
+        std::cout<<"Até 40x de aproximadamente R$ 371,00"<<std::endl;
+        std::cout<<"\nDocentes:"<<std::endl;
+        std::cout<<"- Carol"<<std::endl;
+        std::cout<<"- Thamise"<<std::endl;
+        std::cout<<"\nHorários:"<<std::endl;
+        std::cout<<"- Segunda a sexta"<<std::endl;
+        std::cout<<"- Das 19h às 22h"<<std::endl;
+        std::cout<<"\nou"<<std::endl;
+        std::cout<<"\n- Segunda a sexta"<<std::endl;
+        std::cout<<"- Das 09h às 12h"<<std::endl;
+        std::cout<<"\nCarga Horária:"<<std::endl;
+        std::cout<<"1.600 horas"<<std::endl;
+        std::cout<<"\nGostaria de saber as possíveis formas de formas de pagamento?\n1 - SIM ( EM DESENVOLVIMENTO )\n2 - NÃO "<<std::endl;
+        std::cin>>op;
+
+        switch(op){
+
+            case 1:
+            std::cout<<"Em desenvolvimento"<<std::endl;
+            exit(0);
+
+            case 2:
+            system("cls");
+            return;
+
+            default:
+            std::cout<<"Opção inválida!"<<std::endl;
+        }
+    }while(true);
+}
+void infoTDS(){
+    
+    int op;
+        do{
+        std::cout<<"\n======================================================"<<std::endl;
+        std::cout<<"=== CURSO: Técnico em Desenvolvimento de Sistemas ==="<<std::endl;
+        std::cout<<"====================================================="<<std::endl;
+        std::cout<<"\nValor Total:"<<std::endl;
+        std::cout<<"R$ 14.405,49"<<std::endl;
+        std::cout<<"\nParcelamento:"<<std::endl;
+        std::cout<<"Até 30x de R$ 493,51"<<std::endl;
+        std::cout<<"\nDocentes:"<<std::endl;
+        std::cout<<"- Eduardo"<<std::endl;
+        std::cout<<"\nHorários:"<<std::endl;
+        std::cout<<"- Segunda a sexta"<<std::endl;
+        std::cout<<"- Das 19h às 22h"<<std::endl;
+        std::cout<<"\nCarga Horária:"<<std::endl;
+        std::cout<<"1.216 horas"<<std::endl;
+        std::cout<<"\nGostaria de saber as possíveis formas de formas de pagamento?\n1 - SIM ( EM DESENVOLVIMENTO )\n2 - NÃO "<<std::endl;
+        std::cin>>op;
+
+        switch(op){
+
+            case 1:
+            std::cout<<"Em desenvolvimento"<<std::endl;
+            exit(0);
+
+            case 2:
+            system("cls");
+            return;
+
+            default:
+            std::cout<<"Opção inválida!"<<std::endl;
+        }
+    }while(true);
+}
 void cadastroAluno(){
 
 }
@@ -11,50 +130,90 @@ void cursosEAD(){
 }
 void cursosPresenciais(){
 
+    int op;
+
+    do{
+    std::cout<<"============ Cursos disponíveis na modalidade PRESENCIAL ============"<<std::endl;
+	std::cout<<"Qual curso deseja saber as informações??"<<std::endl;
+	std::cout<<"1 - Técnico em Desenvolvimento de Sistemas"<<std::endl;
+	std::cout<<"2 - Enfermagem"<<std::endl;
+	std::cout<<"3 - Modelagem"<<std::endl;
+    std::cout<<"4 - Voltar"<<std::endl;
+    std::cout<<"5 - Sair"<<std::endl;
+    std::cin>>op;
+
+    switch(op){
+        case 1:
+        infoTDS();
+        break;
+        
+        case 2:
+        infoEnfermagem();
+        break;
+
+        case 3:
+        infoModelagem();
+        break;
+
+        case 4:
+        system("cls");
+        return;
+
+        case 5:
+        std::cout<<"\nSistema Finalizado!!"<<std::endl;
+        exit(0);
+        
+        default:
+        std::cout<<"Opção Inválida!!"<<std::endl;
+    }
+
+    }while(true);
 }
 void menuPrincipal(){
 
-    std::string opcao;
+    int op;
 
     do{
         std::cout<<"\n========= PAINEL SENAC ========="<<std::endl;
         std::cout<<"1 - Cursos Presencias "<<std::endl;
         std::cout<<"2 - Cursos EAD"<<std::endl;
         std::cout<<"3 - Cadastro Aluno"<<std::endl;
-        std::cout<<"4 - Voltar"<<std::endl;
-        std::getline(std::cin, opcao);
+        std::cout<<"4 - Sair"<<std::endl;
+        std::cin>>op;
 
-        switch(opcao[0]){
+        switch(op){
 
-            case '1':
+            case 1:
             cursosPresenciais();
-            return;
+            break;
 
-            case '2':
+            case 2:
             cursosEAD();
-            return;
+            break;
 
-            case '3':
+            case 3:
             cadastroAluno();
-            return;
+            break;
 
-            case '4':
+            case 4:
             std::cout<<"\nSistema Finalizado!!"<<std::endl;
-            return;
+            exit(0);
 
             default:
-            std::cout<<"Opção inválida."<<std::endl;
+            std::cout<<"\nOpção inválida."<<std::endl;
         }
     }while(true);
 }
-
+ 
 
 
 
 int main(){
+    
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
+
     menuPrincipal();
 
     return 0;
