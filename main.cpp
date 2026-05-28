09213bca1a44a9ce82b247168b25e2728b7e8159
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-05-28 19:45:17 -0300

    Logica de fibonacci

--- main.cpp
+++ main.cpp
@@ -4,27 +4,18 @@
 
 int main(){
 
-    std::string nome;
-    int idade;
-    double altura;
-     
-     std::cout<<"Digite o seu nome: "<<std::endl;
-     std::getline(std::cin, nome);
+    int fibonacci[10];
 
-     std::cout<<"Digite a sua idade: "<<std::endl;
-     std::cin>>idade;
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
 
-     std::cout<<"Digite a sua altura: "<<std::endl;
-     std::cin>>altura;
-
-     std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
-
-     std::cout<<"\n ----- Dados salvos na Tupla -----"<<std::endl;
-
-     std::cout<<"Nome: "<<std::get<0>(pessoa)<<std::endl;
-     std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
-     std::cout<<"Altura: "<<std::get<2>(pessoa)<<"m."<<std::endl;
+    for(int i =2; i < 10; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
     
+    for(int i = 0; i < 10; i++){
+        std::cout<<fibonacci[i]<<"\n";
+    }
 
     return 0;
 }
