cf99b2d996e52eb01e5dda22931dd5ef208ed1a9
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-05-28 20:10:33 -0300

    fibonacci 3.0

--- main.cpp
+++ main.cpp
@@ -2,22 +2,19 @@
 #include <tuple>
 #include <string>
 
-void contar (int numero){
-    if(numero == 0){
-        std::cout<<"PARE!!\n";
-        return;
-    }
-    std::cout<<numero<<"\n";
 
-    contar(numero -1 );
+int fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
 
 
 int main(){
 
-    contar(5);
+    std::cout<<fibonacci(7)<<std::endl;
 
     return 0;
 }
-  
+  // 0,1,1,2,3,5,8
