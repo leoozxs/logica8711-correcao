02bffe716de8dbb058be437e0f5ee53123d9207e
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-06-11 19:29:33 -0300

    Torre de Hanoi com Recursão

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
+
+void torredeHanoi(int n, std::string origem, std::string destino, std::string auxiliar){
+        if(n == 1){
+        std::cout<<"Mover disco  "<<origem<<" para "<<destino<<std::endl;
+        return;
+    }
+    torredeHanoi(n - 1, origem, auxiliar, destino);
+    std::cout<<"Mover disco  "<<n<<" de "<<origem<<" para "<<destino<<std::endl;
+    torredeHanoi(n - 1, auxiliar, destino, origem);
+}
+
 int main(){
 
-    int idade = 38;
+    int numDiscos;
+    std::cout<<"Quantos discos: ";
+    std::cin>>numDiscos;
 
-    std::string resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+    std::cout<<"\nSequencia de movimentos: "<<std::endl;
+    torredeHanoi(numDiscos, "A", "C", "B");
 
-    std::cout<<resultado<<std::endl;
-    
+    std::cout<<"\nTotal de movimento: "<<(1 << numDiscos) - 1<<std::endl;
+
+    return 0;
 }
