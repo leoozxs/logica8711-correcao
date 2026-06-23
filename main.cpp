fa56abd23ef7e6a72f2e404d314539762ec3c4a8
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-06-23 20:29:33 -0300

    Pares e impares

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include<iostream>
  
 int main(){
-    int numeros[] = {1,2,3,4,5,6};
-    int tamanho = 8;
-
-    std::cout<<"===== NUMEROS DUPLICADOS =====\n"<<std::endl;
     
-    std::cout<<"Array: ";
+    int numeros[] = {1,2,3,4,5,6,7,8,9,10};
+    int tamanho = 10;
+
+    std::cout<<"===== PARES E IMPARES ====="<<std::endl;
+
+    std::cout<<"\nPares: ";
     for(int i = 0; i < tamanho; i++){
-        std::cout<<numeros[i]<<" ";
+        if(numeros[i] % 2 == 0){
+            std::cout<<numeros[i]<<" ";
+        }
     }
     std::cout<<std::endl;
-
-    std::cout<<"Numeros duplicados: "<<std::endl;
+    std::cout<<"Impares: ";
     for(int i = 0; i < tamanho; i++){
-        for(int j = i + 1; i < tamanho; i++){
-            if(numeros[i] == numeros [j]){
-                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
-                break;
-            }
+        if(numeros[i] % 2 != 0){
+            std::cout<<numeros[i]<<" ";
         }
     }
 
- 
+
     return 0;
 }
