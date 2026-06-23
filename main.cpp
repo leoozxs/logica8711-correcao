e733286571f62a872fdb1fbde25e624bc4b65f5c
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-06-23 19:15:45 -0300

    lifo

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <string>
+
+
+struct Convocados{
+    std::string nome;
+    std::string posicao;
+    int numero;
+};
 
 int main(){
-    int jogos;
 
-    std::cout<<"===== Calculador de público ====="<<std::endl;
-    std::cout<<"\nQuantos jogos:";
-    std::cin>>jogos;
+    Convocados convocados[3];
 
-    int somaPublico = 0;
-
-    for(int i = 1; i <= jogos; i++){
-        int publico;
-        std::cout<<"Publico jogo "<<i<<": ";
-        std::cin>>publico;
-        somaPublico += publico;
-    }
+    std::cout<<"==== CONVOCACAO DA SELECAO ====\n"<<std::endl;
     
-    int media = somaPublico / jogos;
+    for(int i = 0; i < 3; i++){
+        std::cout<<"Jogador "<<(i + 1)<<": "<<std::endl;
+        std::cout<<"Nome: ";
+        std::cin>>convocados[i].nome;
+        std::cout<<"Numero: ";
+        std::cin>>convocados[i].numero;
+        std::cout<<"Posicao: ";
+        std::cin>>convocados[i].posicao;
+    }
+    std::cout<<"===== LISTA DE CONVOCADOS =====\n"<<std::endl;
 
-    std::cout<<"\n===== RESULTADO ====="<<std::endl;
-    std::cout<<"Media de publico: "<<media<<" pessoas"<<std::endl;
-    std::cout<<"Total de público: "<<somaPublico<<" pessoas"<<std::endl;
+    for(int i = 0; i < 3; i++){
+        std::cout<<convocados[i].numero<<" - "<<convocados[i].nome<<" ("<<convocados[i].posicao<<")"<<std::endl;
+    }
 
 }
