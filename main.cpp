8e10a0b937eef657a38b0f60a5210b56901418fc
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-06-23 20:17:35 -0300

    Numeros duplicados

--- main.cpp
+++ main.cpp
@@ -1,61 +1,27 @@
 #include<iostream>
  
 int main(){
- 
-    int fila[10];
-    int inicio = 0;
-    int fim = 0;
-    int op;
- 
-    std::cout<<"===== PLHA COM MENU ====="<<std::endl;
- 
-    while(true){
-        std::cout<<"1 - Enfileirar"<<std::endl;
-        std::cout<<"2 - Desenfilheirar"<<std::endl;
-        std::cout<<"3 - Exibir fila"<<std::endl;
-        std::cout<<"4 - Sair"<<std::endl;
-        std::cout<<"Escolha: ";
-        std::cin>>op;
- 
-        if(op == 1){
-            if(fim < 10){
-                int valor ;
-                std::cout<<"Digite o valor: ";
-                std::cin>>valor;
-                fila[fim] = valor;
-                fim++;
-                std::cout<<"Enfileirado!"<<std::endl;
-            }else{
-                std::cout<<"Fila cheia!"<<std::endl;
-            }
-        }
-        else if(op == 2){
-            if(inicio < fim){
-                std::cout<<"Removido: "<<fila[inicio]<<std::endl;
-            }
-            else{
-                std::cout<<"Fila vazia!"<<std::endl;
-            }
-        }
-        else if(op == 3){
-            if(inicio < fim){
-                std::cout<<"Fila: ";
-                for(int i = inicio; i < fim; i++){
-                    std::cout<<fila[i]<<" ";
-                }
-                std::cout<<std::endl;
-            }else{
-                std::cout<<"Fila vazia!"<<std::endl;
-            }
-        }
-        else if (op == 4){
-            std::cout<<"Saindo..."<<std::endl;
-            break;
-        }
-        
+    int numeros[] = {1,2,3,4,5,6};
+    int tamanho = 8;
 
- 
+    std::cout<<"===== NUMEROS DUPLICADOS =====\n"<<std::endl;
+    
+    std::cout<<"Array: ";
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<numeros[i]<<" ";
     }
+    std::cout<<std::endl;
+
+    std::cout<<"Numeros duplicados: "<<std::endl;
+    for(int i = 0; i < tamanho; i++){
+        for(int j = i + 1; i < tamanho; i++){
+            if(numeros[i] == numeros [j]){
+                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
+                break;
+            }
+        }
+    }
+
  
     return 0;
 }
