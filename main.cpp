428f8bae7f8074f72d193ea5fed9f999ec694cab
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-06-23 19:24:39 -0300

    Fila

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <string>
-
-
-struct Convocados{
-    std::string nome;
-    std::string posicao;
-    int numero;
-};
-
+ 
 int main(){
+   int fila[5];
+   int inicio = 0;
+   int fim = 0;
 
-    Convocados convocados[3];
+    std::cout<<"===== FILA =====\n"<<std::endl;
 
-    std::cout<<"==== CONVOCACAO DA SELECAO ====\n"<<std::endl;
+    fila[0] = 100;
+    fila[1] = 200;
+    fila[2] = 300;
+    fim = 3;
+
+    std::cout<<"Elementos enfileirados: 100, 200, 300\n"<<std::endl;
+
+    std::cout<<"Desenfileirando..."<<std::endl;
+    while (inicio < fim){
+        std::cout<<"Removido: "<<fila[inicio]<<std::endl;
+        inicio++;
+    }
     
-    for(int i = 0; i < 3; i++){
-        std::cout<<"Jogador "<<(i + 1)<<": "<<std::endl;
-        std::cout<<"Nome: ";
-        std::cin>>convocados[i].nome;
-        std::cout<<"Numero: ";
-        std::cin>>convocados[i].numero;
-        std::cout<<"Posicao: ";
-        std::cin>>convocados[i].posicao;
-    }
-    std::cout<<"===== LISTA DE CONVOCADOS =====\n"<<std::endl;
+    
 
-    for(int i = 0; i < 3; i++){
-        std::cout<<convocados[i].numero<<" - "<<convocados[i].nome<<" ("<<convocados[i].posicao<<")"<<std::endl;
-    }
-
+   return 0;
 }
