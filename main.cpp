7d594d9156c2520887376ed08492875ef7dcfb0e
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-07-07 19:31:22 -0300

    Soma o vector e retorna / Exibe o vetor

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@ int main(){
  
     std::vector<int>numeros;
 
-    std::cout<<"===== Limpar vector ====="<<std::endl;
+    std::cout<<"===== Soma e retorna ====="<<std::endl;
 
     numeros.push_back(10);
     numeros.push_back(20);
     numeros.push_back(30);
+    numeros.push_back(40);
+    int soma = 0;
+
+    for(int i = 0; i < 4; i++){
+        soma += numeros[i];
+    }
+    std::cout<<"[ ";
+    for (int n : numeros) {
+    std::cout << n << " ";
+    }
+    std::cout<<"]";
+    std::cout<<"\n"<<soma<<std::endl;
     
-    std::cout<<"Tamanho antes: "<<numeros.size()<<std::endl;
-
-    numeros.clear();
-
-    std::cout<<"Tamanho Depoius: "<<numeros.size()<<std::endl;
-    std::cout<<"Vector vazio?"<<(numeros.empty() ? "Sim" : "Não")<<std::endl;
 
 
     return 0;
